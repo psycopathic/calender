e345004f7a27a757af85063e2f2592620d15c565
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-08-17 03:40:18 +0530

    edit the events

---
+++ app/(main)/(private)/events/[eventId]/edit/page.tsx
@@ -0,0 +1,31 @@
+import EventForm from "@/components/forms/EventForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getEvent } from "@/server/actions/events";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function EditEventPage({
+  params,
+}: {
+  params: Promise<{ eventId: string }>;
+}) {
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId) return redirectToSignIn();
+
+  const { eventId } = await params;
+  const event = await getEvent(userId, eventId);
+  if (!event) return <h1>Event not found</h1>;
+
+  return (
+    <Card className="max-w-md mx-auto border-4 border-blue-100 shadow-2xl shadow-accent-foreground">
+      <CardHeader>
+        <CardTitle>Edit Event</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {/* Render the EventForm with the event details, passing the event data as props */}
+        <EventForm
+          event={{ ...event, description: event.description || undefined }} // If description is null, pass undefined
+        />
+      </CardContent>
+    </Card>
+  );
+}
